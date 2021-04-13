52e7b48ecb37b41783bb66566d505d133e1e187a
Lauren D'Arinzo <lauren.darinzo@nyu.edu>
2021-04-13 17:55:42 -0400

    additional documentation

--- learners.py
+++ learners.py
@@ -120,17 +120,6 @@ rf = RegressionForest(honest=True, random_state=42)
 # initialize metalearner
 S = s_learner(mu_base=rf)
 
-# Set root directory
-base_repo_dir = pathlib.Path(os.path.realpath(__file__)).parents[0]
-
-# Read in data
-train = pd.read_parquet(base_repo_dir / 'data' / 'simA' / 'samp1_train.parquet')
-test = pd.read_parquet(base_repo_dir / 'data' / 'simA' / 'samp1_test.parquet')
-X_train = train.drop(columns=['treatment', 'Y', 'tau'])
-y_train = train['Y']
-W_train = train['treatment']
-X_test = test.drop(columns=['treatment', 'Y', 'tau'])
-
 # Fit S-learner
 X_W = pd.concat([X_train, W_train], axis=1)
 S.fit(X_W=X_W, y=y_train)
@@ -150,8 +139,6 @@ class x_learner:
     From Rosenberg Slides:
         µ^0(x) = E[Y (0) | X = x]
         µ^1(x) = E[Y (1) | X = x]
-
-
         τˆx (x) = g(x)t_0ˆ(x) − (1-g(x))t_1ˆ(x)
 
     authors: Kelsey Markey and Lauren D'Arinzo, April 4 2021
@@ -165,27 +152,30 @@ class x_learner:
         self.tau1_base = clone(tau1_base, safe=False)
 
     def fit(self, X, W, y, fit_g):
+        '''
+        fit_g : boolean indicator if g should be fit to training data. if false, g must be passed explicitlly to x_learner.predict()
+        '''
         # Call fit method
         self.mu0_base.fit(X[W==0], y[W==0])
         self.mu1_base.fit(X[W==1], y[W==1])
     
-        #impute y0 for treated group using mu0
+        #Impute y0 for treated group using mu0
         y0_treat=self.mu0_base.predict(X[W==1]).flatten()
-        #impute y1 for control group using mu1
-        y1_control=self.mu1_base.predict(X[W==0]).flatten()
-        #print("y1_control", y1_control)
-        #print("y0_treat", y0_treat)
-        #print("unique y1", np.unique(y1_control))
-        #### y1_control is all the same value, needs to be debugged! #####
-       
-        imputed_TE_control = y1_control - y[W==0] 
         imputed_TE_treatment = y[W==1] - y0_treat
 
+        #Impute y1 for control group using mu1
+        y1_control=self.mu1_base.predict(X[W==0]).flatten()
+        imputed_TE_control = y1_control - y[W==0] 
+
+        #Fit tau0: CATE estimate fit to the control group
         self.tau0_base.fit(X[W==0], imputed_TE_control)
+
+        #Fit tau1: CATE estimate fit to the treatment group
         self.tau1_base.fit(X[W==1], imputed_TE_treatment)
 
         if fit_g:
             print('X Learner with g fitted')
+            #predict propensities from empirical data
             self.g_fit = LogisticRegression(fit_intercept=True, max_iter=2000).fit(
             X=X, y=W)
         
@@ -195,54 +185,43 @@ class x_learner:
 
 
     def predict(self, X, g):
-        #g will be none if fit_g = True
+        '''
+        g : weight vector that should be length of the test set. can be passed as None if g was fit to data
+        '''
         tau0_preds = self.tau0_base.predict(X).flatten()
         tau1_preds = self.tau1_base.predict(X).flatten()
-        #idea: allow g to be either be a vector or function?
-        #if function: think about sklearn inputs (.predict) vs lamba functions (g(x))
-        #g_typ
+    
     
         if self.g_fit == None:
             tau_preds = (g.T * tau0_preds) + ((1-g).T*tau1_preds)
         else:
             g_preds = self.g_fit.predict_proba(X)[:, 1]
             tau_preds = (g_preds.T * tau0_preds) + ((1-g_preds).T*tau1_preds)
-        #if g_type == 'Function':
+
+        # idea: allow g to be either be a vector or function?
+        # if function: think about sklearn inputs (.predict) vs lamba functions (g(x))
+        # if g_type == 'Function':
         #    g_preds = g(X)
         #    tau_preds = (g_preds.T * tau0_preds) + ((1-g_preds).T*tau1_preds)
 
-        #print("g transpose shape: ", self.g.T.shape)
-        #print("tau0_preds shape: ", tau0_preds.shape)
-        #print("tau1_preds shape: ", tau1_preds.shape)
-        #print("tau preds shape: ", tau_preds.shape)
         return tau_preds.flatten()
 
-# Set root directory
-base_repo_dir = pathlib.Path(os.path.realpath(__file__)).parents[0]
-
-# Read in data
-train = pd.read_parquet(base_repo_dir / 'data' / 'simA' / 'samp1_train.parquet')
-test = pd.read_parquet(base_repo_dir / 'data' / 'simA' / 'samp1_test.parquet')
-X_train = train.drop(columns=['treatment', 'Y', 'tau'])
-y_train = train['Y']
-W_train = train['treatment']
-X_test = test.drop(columns=['treatment', 'Y', 'tau'])
-
 # intialize base learner
-# bart = SklearnModel() # takes too long
+# bart = SklearnModel() 
 rf = RegressionForest(honest=True, random_state=42)
 
 # initialize metalearner
-#keeping g static for now as 0.5 
-#print(g)
-#print(len(g))
 X_learner = x_learner(mu0_base=rf, mu1_base=rf, tau0_base=rf, tau1_base=rf)
+
+# initiliaze g
+g = np.full((len(X_test)), 0.01)
+#g = LogisticRegression(fit_intercept=True, max_iter=2000).fit(
+#       X=X_train, y=W_train)
+
 # Fit treatment and response estimators mu0 and  mu1
 X_learner.fit(X=X_train, W=W_train, y=y_train, fit_g=False)
-g=np.full((len(X_test)), 0.01)
-#g = LogisticRegression(fit_intercept=True, max_iter=2000).fit(
-#        X=X_train, y=W_train)
-#read more about max_iter param, issues with convergence when left to default
+
+# Predict test-set CATEs
 tau_preds = X_learner.predict(X=X_test, g=g)
 
 # Calculate RMSE on test set
